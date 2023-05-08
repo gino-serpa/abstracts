07a74b3d3dde04c6d3ba4d3529218eaed8f41e30
gino serpa <gino.serpa@gmail.com>
2023-05-07 20:16:30 -0400

    modified local library aa so it can find the data

--- abstracts_analysis.py
+++ abstracts_analysis.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 def load_to_df(choice='short'):
 
-    filenames = [Path("../data/arxiv_data.csv"), 
-             Path("../data/arxiv_data_210930-054931.csv")]
+    filenames = [Path("data/arxiv_data.csv"), 
+             Path("data/arxiv_data_210930-054931.csv")]
     if choice=='short':
         print('Loading ', filenames[0])
         df = pd.read_csv(filenames[0])
