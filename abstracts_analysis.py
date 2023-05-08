5cd0822711e3fce0f083c66877315b69dee6e126
gino serpa <gino.serpa@gmail.com>
2023-05-07 20:13:05 -0400

    first commit with content

---
+++ abstracts_analysis.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+from pathlib import Path
+
+
+def load_to_df(choice='short'):
+
+    filenames = [Path("../data/arxiv_data.csv"), 
+             Path("../data/arxiv_data_210930-054931.csv")]
+    if choice=='short':
+        print('Loading ', filenames[0])
+        df = pd.read_csv(filenames[0])
+    else:
+        print('Loading ', filenames[1])
+        df = pd.read_csv(filenames[1])
+
+    return df
+
